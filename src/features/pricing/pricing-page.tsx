9fccc1faf9bda430dd0ee0a60bf451d9efb1ac1c
Hashir <hashirventhodi@gmail.com>
2025-12-28 22:06:38 +0530

    fix(pricing): remove _backup folder and use comment approach

--- src/features/pricing/pricing-page.tsx
+++ src/features/pricing/pricing-page.tsx
@@ -1,11 +1,18 @@
 import PricingHero from "./components/PricingHero"
 import PricingCards from "./components/PricingCards"
+// TODO: Uncomment when pricing is finalized
+// import ComparisonTable from "./components/ComparisonTable"
+// import PricingFAQ from "./components/PricingFAQ"
 
 export default function PricingPage() {
   return (
     <>
       <PricingHero />
       <PricingCards />
+      {/* TODO: Uncomment when pricing is finalized
+      <ComparisonTable />
+      <PricingFAQ />
+      */}
     </>
   )
 }
